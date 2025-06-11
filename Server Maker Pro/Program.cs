00804873fc936dbfc2a12e93e1328645eaab5ea6
Milizzo <milo.b.lee@gmail.com>
2025-06-11 18:01:48 -0400

    first commit

---
+++ Server Maker Pro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Server_Maker_Pro
+{
+    static class Program
+    {
+        static void Main()
+        {
+            FirstSetup();
+
+            
+        }
+
+        static void FirstSetup()
+        {
+            string? path = null;
+
+            while (path == null || !Directory.Exists(path))
+            {
+                Console.WriteLine("Path to server files: ");
+                path = Console.ReadLine() ?? string.Empty;
+            }
+        }
+    }
+}
