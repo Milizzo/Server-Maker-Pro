4f8e56bf32513af8a3339598cd5ab7c6622d5c14
Milizzo <milo.b.lee@gmail.com>
2025-06-11 18:38:00 -0400

    Update main logic to include menus

--- Server Maker Pro/Program.cs
+++ Server Maker Pro/Program.cs
@@ -5,10 +5,67 @@ namespace Server_Maker_Pro
 {
     static class Program
     {
+        static string serversPath = string.Empty;
+
         static void Main()
         {
-            FirstSetup();
+            // FirstSetup();
+            serversPath = "/Users/milol/Documents/";
 
+            string[] thingsToDo =
+            [
+                "play",
+                "create",
+                "exit",
+            ];
+
+            while (true)
+            {
+                string response = AskForOptions(thingsToDo);
+
+                if (response == "play")
+                {
+                    ServerSelection();
+                }
+                if (response == "create")
+                {
+                    Console.WriteLine("Nice try!");
+                }
+                else if (response == "exit")
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+            }
+        }
+
+        static void ServerSelection()
+        {
+            string[] files = Directory.GetDirectories(serversPath);
+
+            if (files.Length <= 0)
+            {
+                Console.WriteLine("No servers available!");
+                return;
+            }
+
+#pragma warning disable CS8601 // Possible null reference assignment.
+            files = [.. files.Select(Path.GetFileName)];
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+            string response = AskForOptions(files, "Select a server (by name): ");
+
+            if (!Directory.Exists(Path.Combine(serversPath, response)))
+            {
+                System.Console.WriteLine($"Server could not be found: {response}");
+                return;
+            }
+
+            ServerMenu(response);
+        }
+
+        static void ServerMenu(string server)
+        {
             
         }
 
@@ -16,11 +73,30 @@ namespace Server_Maker_Pro
         {
             string? path = null;
 
-            while (path == null || !Directory.Exists(path))
+            while (string.IsNullOrWhiteSpace(path))
             {
-                Console.WriteLine("Path to server files: ");
+                Console.Write("Path to server folders: ");
                 path = Console.ReadLine() ?? string.Empty;
             }
+
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            serversPath = path;
+        }
+
+        static string AskForOptions(string[] options, string question = "Choose an option: ")
+        {
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.WriteLine(/*(optionPrefix != null ? (i + 1).ToString() + optionPrefix : "") + */"- " + options[i]);
+            }
+
+            string? currentResponse = null;
+            while (currentResponse == null || !options.Contains(currentResponse))
+            {
+                Console.Write(question);
+                currentResponse = Console.ReadLine() ?? string.Empty;
+            }
+            return currentResponse;
         }
     }
 }
